6046de759be031f475979dc87b72b9e49d333dd6
prince <prabh.ubhi7042@gmail.com>
2024-09-11 18:47:23 +0530

    add butterfly

--- homework/butterfly.cpp
+++ homework/butterfly.cpp
@@ -10,22 +10,40 @@ int main() {
         {
             cout<<"*" <<" ";
         }
-        // cout<<endl;
-        for (int j = 0; j < 2*(n-i)-5; j++)
+        //top spaces
+        for (int j = 0; j < 2*(n-i-1) ; j++)
         {
-            cout<<" ";
+            cout<<" " << " ";
         }
-        cout<<"*";
+
+        for (int j = 0; j < i+1; j++)
+        {
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+        //top right numbers
+    }
+
+
+    //bottom left triangle
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            cout<<"*"<<" ";
+        }
+        //bottom spaces
+        for (int j = 0; j < 2*i; j++)
+        {
+            cout << " "<<" ";
+        }
+        //right triangle 
+        for (int j = 0; j < n-i; j++)
+        {
+            cout << "*" <<" ";
+        }
+        
         cout<<endl;
     }
-    //bottom left numbers
-    // for (int i = 0; i < n; i++)
-    // {
-    //     for (int j = i; j < n; j++)
-    //     {
-    //         cout<<"*"<<" ";
-    //     }
-    //     cout<<endl;
-    // }
     return 0;
 }
