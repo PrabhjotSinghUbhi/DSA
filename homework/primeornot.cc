3fa88e7c64e3c4a979ced8e653b7f48de9854e3c
prince <prabh.ubhi7042@gmail.com>
2024-09-12 07:50:26 +0530

    edited folders for clang installation

--- homework/primeornot.cc
+++ homework/primeornot.cc
@@ -1,33 +1,35 @@
-// function to determine prime or not
+// function to determine prime.
 #include <iostream>
 using namespace std;
 
-void p_or_nt(int n);
+int checkprime(int n);
 
-int main() {
-    cout << p_or_nt(5);
+int main()
+{
+    cout << checkprime(9) << endl;
     return 0;
 }
 
 
 
-
-
-void p_or_nt(int n){
+int checkprime(int n)
+{
     bool isprime = true;
-    for (int i = 2; i < n-1 ; i++)
+    for (int i = 2; i <= n-1; i++)
     {
-        if (n%i == 0){
+        if (n%i == 0)
+        {
             isprime = false;
         }
-        if (isprime==true)
-        {
-            cout << "prime";
-        }
-        else{
-            cout << "not prime";
-        }
-        
+    }
+    if (isprime == true )
+    {
+        cout << "prime";
+    }
+    else{
+        cout << "not prime";
     }
     
+    return 0;
 }
+
--- lec1/snipet.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    char value= 'a';
-    int b = value;
-    cout<<b<<endl;
-    return 0;
-}
